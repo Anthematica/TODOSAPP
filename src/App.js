0a897c3a91628a6e276b0a9e7038bb42eb1f22d7
Anthematica <fernando21112000@gmail.com>
2022-02-06 18:14:17 -0600

    Estilizando el proyecto

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import { TodoCounter } from "./TodoCounter"
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
+import { TodoSearch } from './TodoSearch';
 
 const todos = [
   {text: "Jugar Clash Royal", completed: false},
   {text: "Estudiar React", completed: true},
+  {text: "Estilizar mi Todo", completed: true},
+  {text: "Estudiar Ingles", completed: true},
 ];
 
 function App() {
@@ -16,11 +19,15 @@ function App() {
     <React.Fragment>
       <TodoCounter />
 
-      {/* <TodoSearch /> */}
-      <input placeholder="Cebolla" />
+      <TodoSearch /> 
+
       <TodoList>
         {todos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text}/>
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text}
+            completed = {todo.completed}
+          />
         ))}
       </TodoList>
 
